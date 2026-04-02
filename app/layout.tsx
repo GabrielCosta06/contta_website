13fe58051d4e8fc1dd847b20ab9ce32b0590c3cc
Gabriel <josetropilo@gmail.com>
2026-04-01 22:30:26 -0300

    Force light theme on mobile browsers

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#134176",
+  themeColor: "#f8fbff",
+  colorScheme: "light",
 };
 
 export default function RootLayout({
